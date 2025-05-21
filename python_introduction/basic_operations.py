98c1a8a7c63b671dd38a1f2867024c5563796f47
muekep <c.mueke@outlook.com>
2025-05-21 23:59:40 +0300

    Initial Commit

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+result1 = number1 + number2
+print("Addition of "+ str(number1) +" and "+str(number2)+" is "+str(result1))
+result2 = number1 - number2
+print("Substraction of "+ str(number1) +" and "+str(number2)+" is "+str(result2))
+result3 = number1 * number2
+print("Multiplication of "+ str(number1) +" and "+str(number2)+" is "+str(result3))
