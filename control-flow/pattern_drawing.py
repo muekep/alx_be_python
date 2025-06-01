b3ed7a0359b02bb440f061a6044cfbc091245d55
muekep <63904395+muekep@users.noreply.github.com>
2025-06-01 15:41:50 +0300

    pattern drawing

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,7 +1,6 @@
 while True:
     try:
-        size_str = input("Enter the size of the pattern: ")
-        size = int(size_str)
+        size = int(input("Enter the size of the pattern: "))
 
         if size <= 0:
             print("Please enter a positive integer.")
