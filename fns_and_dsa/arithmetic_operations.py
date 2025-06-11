6a469fdbede037e9ad587a38d99b9e94c2b8a0bc
muekep <63904395+muekep@users.noreply.github.com>
2025-06-11 23:24:41 +0300

    changes

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,20 +1,22 @@
 def perform_operation(num1, num2, operation):
-    match operation:
-            case "add":
-                return  num1 + num2
+    if operation = "add":
+        return  num1 + num2
                 
-            case "subtract":
-              return num1 - num2
+    elif:
+        operation "subtract"
+        return num1 - num2
               
-            case "multiply":
-              return  num1 * num2
+    elif:
+        operation = "multiply":
+        return  num1 * num2
               
-            case "divide":
+    elif:
+        operation = "divide":
                 if num2 == 0:
                     return "Error: Division by zero is not allowed."
                 else:
                     return num1 / num2
                 
-            case _:
-                return "Invalid operation. Please choose from add, subtract, multiply, division."
+    else:
+        return "Invalid operation. Please choose from add, subtract, multiply, division."
  
