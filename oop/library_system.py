7adf1a556525a6bf1c1987d29f2e0e20fc425df4
muekep <63904395+muekep@users.noreply.github.com>
2025-06-24 00:41:49 +0300

    library_system

--- oop/library_system.py
+++ oop/library_system.py
@@ -13,7 +13,7 @@ class PrintBook(Book):
 class Library:
   def __init__(self):
     def add_book(self, book: Book):
-    if not isinstance(book, Book):
+      if not isinstance(book, Book):
         print(f"Error: Cannot add non-Book object to the library: {type(book)}")
         return
     self.books = [], append()
