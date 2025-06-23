7084b9f40c2c8cdf9dc884d8680bafff417a37e5
muekep <63904395+muekep@users.noreply.github.com>
2025-06-24 00:37:45 +0300

    library_system

--- oop/library_system.py
+++ oop/library_system.py
@@ -8,8 +8,8 @@ class EBook(Book):
     self.file_size = file_size
 class PrintBook(Book):
   def __init__(self, title: str, author: str, page_count: int):
-      super().__init__(title, author)
-      self.page_count = page_count
+    super().__init__(title, author)
+    self.page_count = page_count
 class Library:
   def __init__(self):
   def add_book(self, book: Book):
@@ -22,12 +22,12 @@ class Library:
         """
         Prints details of each book currently in the library's collection.
         """
-        if not self._books:
-            print("The library currently has no books.")
-            return
+      if not self._books:
+          print("The library currently has no books.")
+          return
 
-        print("\n--- All Books in Library ---")
+      print("\n--- All Books in Library ---")
         for i, book in enumerate(self._books, 1):
             print(f"{i}. {book}") # Uses the __str__ method of each book object
-        print("----------------------------")
+      print("----------------------------")
   
