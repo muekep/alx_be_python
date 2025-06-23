aed219bc62c3088928ba27edb8e602b5b2a3c211
muekep <63904395+muekep@users.noreply.github.com>
2025-06-24 00:24:03 +0300

    library_system

---
+++ oop/library_system.py
@@ -0,0 +1,31 @@
+class Book:
+  def __init__(self, title: str, author:str):
+    self.title=title
+    self.author=author
+class EBook(Book):
+  def __init__(self, title: str, author: str, file_size: int):
+    super().__init__(title, author)
+    self.file_size = file_size
+class PrintBook(Book):
+  def __init__(self, title: str, author: str, page_count: int):
+      super().__init__(title, author)
+      self.page_count = page_count
+class Library:
+  def __init__(self):
+  def add_book(self, book: Book):
+    if not isinstance(book, Book):
+        print(f"Error: Cannot add non-Book object to the library: {type(book)}")
+        return
+  def list_books(self):
+        """
+        Prints details of each book currently in the library's collection.
+        """
+        if not self._books:
+            print("The library currently has no books.")
+            return
+
+        print("\n--- All Books in Library ---")
+        for i, book in enumerate(self._books, 1):
+            print(f"{i}. {book}") # Uses the __str__ method of each book object
+        print("----------------------------")
+  
